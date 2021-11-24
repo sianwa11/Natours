c676ba518d306494d09c967a8ea8d733a07b9ace
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2021-11-24 08:20:11 +0300

    Completed server side rendering with pug

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,15 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
+import { updateSettings } from './updateSettings';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+// const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form-login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // DELEGATION
 if (mapBox) {
@@ -22,6 +26,39 @@ if (loginForm) {
     const password = document.getElementById('password').value;
     e.preventDefault();
     login(email, password);
+  });
+}
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', e => {
+    // Values
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    e.preventDefault();
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    // Values
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save Password';
+
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
 
