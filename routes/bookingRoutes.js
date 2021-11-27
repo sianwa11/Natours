40f5522bbeeda97ddc695426e411339560452584
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2021-11-27 22:50:04 +0300

    Setting up stripe & completing the bookings api

---
+++ routes/bookingRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bookingController = require('./../controllers/bookingController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+// API routes
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router.route('/:id').get(bookingController.getBooking);
+
+module.exports = router;
