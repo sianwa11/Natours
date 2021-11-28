696f2538c6c2ae82b2a0a8e3cc621d459f2e3ed4
sianwa11 <33891397+sianwa11@users.noreply.github.com>
2021-11-28 13:51:49 +0300

    Improved stripe implementation 4

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -12,6 +12,32 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    mode: 'payment',
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`
+            ]
+          }
+        }
+      }
+    ]
+  });
+  /*   const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
     //TODO: NOTE! In live stripe uses webhooks instead of this
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
@@ -50,7 +76,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         }
       }
     ]
-  });
+  }); */
 
   // 3) Create session as a response
   res.status(200).json({
